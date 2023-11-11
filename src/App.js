8c5b8ab2a9a91f73ec4a5b8c700bcfcc5b5cd7f7
Harshit chopde <109473586+Harshitchopde@users.noreply.github.com>
2023-11-11 16:18:00 +0530

    add some more files

--- src/App.js
+++ src/App.js
@@ -2,37 +2,42 @@ import {
   BrowserRouter,
   Routes,
   Route,
- 
+
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import Login from "./pages/login/Login";
+
+// import { ThemeProvider } from "styled-components";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home/>}/>
-          <Route path="login" element={<Login/>}/>
-          <Route path="users">
-            <Route index element = {<List/>}/>
-            <Route path="new" element={<New/>}/>
-            {/*  users/:userId */}
-            <Route path=":userId" element = {<Single/>}/>
-          </Route>
-          <Route path="products">
-            <Route index element = {<List/>}/>
-            <Route path="new" element={<New/>}/>
-            <Route path=":productId" element = {<Single/>}/>
-          </Route>
+      {/* <ThemeProvider> */}
 
-        </Route>
-      </Routes>
-      </BrowserRouter>
-     
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="users">
+                <Route index element={<List />} />
+                <Route path="new" element={<New />} />
+                {/*  users/:userId */}
+                <Route path=":userId" element={<Single />} />
+              </Route>
+              <Route path="products">
+                <Route index element={<List />} />
+                <Route path="new" element={<New />} />
+                <Route path=":productId" element={<Single />} />
+              </Route>
+
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      {/* </ThemeProvider> */}
+
     </div>
   );
 }
